592117c5e2231bdd4ef0be099fdced40b26ca40b
Richard Greenwood <richardsgreenwood@gmail.com>
2013-06-24 17:28:09 -0700

    add some new libraries and presets

--- init.py
+++ init.py
@@ -3,8 +3,8 @@ import nuke
 #PLUGIN PATHS
 nuke.pluginAddPath('./gizmos')
 nuke.pluginAddPath('./python')
+nuke.pluginAddPath('./tcl')
 nuke.pluginAddPath('./icons')
-nuke.pluginAddPath('./python')
 nuke.pluginAddPath('./lib')
 nuke.pluginAddPath('./lut')
 
@@ -27,8 +27,15 @@ nuke.knobDefault('Root.proxy_type', 'scale')
 nuke.knobDefault('Root.proxy_scale', '.5')
 nuke.knobDefault('Root.fps', '23.976')
 
+# NODE PRESETS
+# ////////////////////////////////////////////////////////////////////////////////
+import cam_presets
+cam_presets.nodePresetCamera()
+import reformat_presets
+reformat_presets.nodePresetReformat()
+
 # LUTs
-#AA 2
+#Example
 #nuke.knobDefault('Viewer.viewerProcess', 'rec709')
 #nuke.knobDefault('monitorLut', 'rec709')
 #nuke.knobDefault('floatLut', 'rec709')
--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import nuke, pipeline, setProjectFrameRange
+import nodeCount
 
 # TOOLS
 # ////////////////////////////////////////////////////////////////////////////////
@@ -62,14 +63,23 @@ m=menubar.addMenu("&Render")
 #m.addSeparator(index=5)
 #m.addCommand("Submit to Smedge", "SmedgeRender.SmedgeRender()", "^F5", index=6)
 
-m = menubar.addMenu("Import and Export")
+m = menubar.addMenu("Extra", index=6)
 # Nuke2Maya
 import FromNuke2MayaExporter, FromMaya2NukeImporter
 m.addCommand("Export Camera as fm2n-File", "FromNuke2MayaExporter.FromNuke2MayaExporter()")
 m.addCommand("Import fm2n-File", "FromMaya2NukeImporter.FromMaya2NukeImporter()")
 #Collect Files Menu Node
+m.addSeparator()
 import collectFiles
 m.addCommand('Collect Files', 'collectFiles.collectFiles()')
+#reveal in OS
+import revealInOS
+m.addCommand('Reveal In Finder','revealInOS.revealInOS()', icon='Read.png')
+m.addSeparator()
+#Metadata
+import showMetaData
+m.addCommand("Show MetaData","nuke.display('showMetaData.showMeta()', nuke.selectedNode(),'MetaData at ' + nuke.selectedNode().name(), 1000)","ctrl+m")
+m.addCommand("Mirror Nodes x", "nuke.tcl('MirrorNodePos x')")
 
 # PANELS
 # ////////////////////////////////////////////////////////////////////////////////
