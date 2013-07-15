f8f32ec7c60308437e267186a14194512e450aa2
Richard Greenwood <richardsgreenwood@gmail.com>
2013-07-15 12:34:51 -0700

    add headoffset

--- python/setProjectFrameRange.py
+++ python/setProjectFrameRange.py
@@ -1,8 +1,8 @@
 # @title setProjectFrameRange
 # @description Prompts the user for received and deliverable handle ranges on the selected read node to calculate project frame range
 # @author Richard Greenwood
-# @version 1.0
-# @compatible Nuke6v3
+# @version 1.1
+# @compatible Nuke7
 # @url www.richardgreenwood.ca
 
 import nuke, nukescripts
@@ -15,6 +15,7 @@ if nuke.env["gui"]:
             handlesInTail = 48
             handlesOutHead = 8
             handlesOutTail = 8
+            headOffset = 1
 
             #create dialogue box
             nukescripts.PythonPanel.__init__( self, "Set Project Frame Range" )
@@ -27,6 +28,8 @@ if nuke.env["gui"]:
             self.handlesOutHead = nuke.Int_Knob ( "handlesOutHead", "Head" )
             self.handlesOutTail = nuke.Int_Knob ( "handlesOutTail", "Tail" )
             self.handlesOutTail.clearFlag( nuke.STARTLINE )
+            self.headOffsetText = nuke.Text_Knob ( "headOffsetText", "Offset" )
+            self.headOffset = nuke.Int_Knob ( "headOffset", "Head" )
             #add knobs
             self.addKnob ( self.handlesInText )
             self.addKnob ( self.handlesInHead )
@@ -34,17 +37,20 @@ if nuke.env["gui"]:
             self.addKnob ( self.handlesOutText )
             self.addKnob ( self.handlesOutHead )
             self.addKnob ( self.handlesOutTail )
+            self.addKnob ( self.headOffsetText )
+            self.addKnob ( self.headOffset )
             #populate knobs
             self.handlesInHead.setValue( handlesInHead )
             self.handlesInTail.setValue( handlesInTail )
             self.handlesOutHead.setValue( handlesOutHead )
             self.handlesOutTail.setValue( handlesOutTail )
+            self.headOffset.setValue ( headOffset )
 
         #shows as modal dialogue (adds 'ok' and 'cancel' buttons)
         def showPanel( self ):
             result = nukescripts.PythonPanel.showModalDialog( self )
             if result:
-                setFrameRange( self.handlesInHead.value(), self.handlesInTail.value(), self.handlesOutHead.value(), self.handlesOutTail.value())
+                setFrameRange( self.handlesInHead.value(), self.handlesInTail.value(), self.handlesOutHead.value(), self.handlesOutTail.value(), self.headOffset.value() )
 
 # Returns setProjectFrameRange dialogue
 def callPanel():
@@ -56,14 +62,14 @@ def callPanel():
         nuke.message("Selected node must be a Read node.")
 
 # Calculates and sets project frame ranges from a selected read node
-def setFrameRange(hInHead = 48, hInTail = 48, hOutHead = 8, hOutTail = 8):
+def setFrameRange(hInHead = 48, hInTail = 48, hOutHead = 8, hOutTail = 8, headOffset = 1):
     plate = nuke.selectedNode()
-    # starts plate at frame 1
+    # starts plate at frame 1000
     plate["frame_mode"].setValue(1)
-    plate["frame"].setValue("1")
+    plate["frame"].setValue(str(headOffset))
     # calculates frame range
-    firstFrame = str(1+hInHead-hOutHead)
-    lastFrame = str(1+plate["last"].getValue()-plate["first"].getValue()-hInTail+hOutTail)
+    firstFrame = str(headOffset+hInHead-hOutHead)
+    lastFrame = str(headOffset+plate["last"].getValue()-plate["first"].getValue()-hInTail+hOutTail)
     #sets project frame range
     nuke.knob("root.first_frame",firstFrame)
     nuke.knob("root.last_frame",lastFrame)
